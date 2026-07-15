d4e6ed1b48e8f06fa585ce7e22253ae4c88f62c9
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-15 17:57:58 +0300

    Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { assets } from '../assets'
+import { assets, menuLinks } from '../assets/assets'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+
+const Location = useLocation();
+
   return (
     <div>
-        <img src = {assets.logo} alt = "logo"  className="h-8"/>
+        <Link to = "/">
+            <img src = {assets.logo} alt = "logo"  className="h-8"/>
+        </Link>
+
+        <div className={`max-sm:fixed max-sm:h-screen max-sm:w-full max-sm:top-16 max-sm:border-t border-borderColor right-0 flex flex-col
+                        sm:flex-row items-start sm:items-center gap-4 sm:gap-8 max-sm:gap-4 transition-all duration-300 z-50 ${Location.pathname
+                        === "/" ? "bg-light" : "bg-white"}`}>
+            {menuLinks.map((link, index) => (
+                <Link key={index} to={link.path}>
+                    {link.name}
+                </Link>
+            ))}
+        </div>
     </div>
   )
 }
