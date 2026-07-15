d4ddfd9acf03df219d0bc32992ee6cb55a31b76c
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-15 16:37:50 +0300

    creation of component $ pages folders and wraping the browserouter in main

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { assets } from '../assets'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img src = {assets.logo} alt = "logo"  className="h-8"/>
+    </div>
+  )
+}
+
+export default Navbar
