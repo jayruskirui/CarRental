15dd56e288733c682c8027d1edc15a5e74f81ca7
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-19 16:09:10 +0300

    creation of carcard component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import CarCard from '../components/CarCard'
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <CarCard/>   
     </>
   )
 }
