8239ba0128df2a6c0052facd6b29e2b195887f04
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-20 16:56:50 +0300

    creation of footer and mounting it on app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -23,6 +24,7 @@ const isOwnerPath = useLocation().pathname.startsWith('/owner');
       <Route path = "/my-bookings" element = {<MyBookings />} />
     </Routes>
 
+    {!isOwnerPath && <Footer />}
 
     </>
   )
