2c08beb856473bd7ba6d5af597e670f558faa960
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-23 00:44:25 +0300

    creating pages component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
 import Footer from './components/Footer'
+import Layout from './pages/owners/Layout'
+import Dashboard from './pages/owners/Dashboard'
+import AddCar from './pages/owners/AddCar'
+import ManageCars from './pages/owners/ManageCars'
+import ManageBookings from './pages/owners/ManageBookings'
 
 const App = () => {
 
@@ -22,6 +27,12 @@ const isOwnerPath = useLocation().pathname.startsWith('/owner');
       <Route path = "/car-details/:id" element = {<CarDetails/>} />
       <Route path = "/cars" element = {<Cars />} />
       <Route path = "/my-bookings" element = {<MyBookings />} />
+      <Route path = "/owners" element = {<Layout />}>
+          <Route index element ={<Dashboard/>}/>
+          <Route path='add-car' element ={<AddCar/>}/>
+          <Route path='manage-cars' element ={<ManageCars/>}/>
+          <Route path='manage-bookings' element ={<ManageBookings/>}/>
+      </Route>
     </Routes>
 
     {!isOwnerPath && <Footer />}
