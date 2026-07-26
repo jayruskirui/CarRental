ba84ba43e5a6b2215d1e59f9907fb9e6667e3e18
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-26 23:42:50 +0300

    correcting bugs

--- client/src/pages/owners/Layout.jsx
+++ client/src/pages/owners/Layout.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import Sidebar from "../../components/owner/Sidebar";
-import NavbarOwner from "../../components/owner/NavbarOwner";
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../../components/owner/Sidebar'
+import NavbarOwner from '../../components/owner/NavbarOwner'
 
 const Layout = () => {
   return (
-    <div>
+    <div className='min-h-screen bg-gray-50'>
       <NavbarOwner />
-      <Sidebar />  
+      <div className='flex'>
+        <Sidebar />
+        <main className='flex-1'>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
