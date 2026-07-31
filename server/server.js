949f0d2ef17cbb77914fb5b52c8933ea8e88f00a
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-07-31 23:28:44 +0300

    connecting server with db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 //Initialize Express App
 const app = express()
+
+//connect database
+await connectDB()
+
 
 //Middleware
 app.use(cors());
