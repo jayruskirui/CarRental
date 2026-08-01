4b4f0a48ffdde5733f0b14c167aa3d7c99f84ba7
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-01 22:12:38 +0300

    debugging

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
+import { setServers } from "node:dns/promises";
+setServers(["1.1.1.1"]);
+
+
 const connectDB = async () => {
     try {
-      mongoose.connection.on('connected', ()=> console.log('Database connected💯'));
-      await mongoose.connect (`${process.env.MONGODB_URI}/CarRental`)  
+      mongoose.connection.on('connected', ()=> console.log("Database connected💯"));
+      await mongoose.connect (`${process.env.MONGODB_URI}/CarRental`)
+       
     } 
     catch (error) {
         console.log(error.message)
