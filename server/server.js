d07281d925e5c2e3bcd97fa9a616eef54b990f99
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-02 23:11:42 +0300

    creating the owner api

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
 
 //Initialize Express App
 const app = express()
@@ -18,6 +19,8 @@ app.use(express.json());
 
 app.get('/', (req, res)=>res.send("Server is running"))
 app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>console.log(`Server is running on port ${PORT}`))
