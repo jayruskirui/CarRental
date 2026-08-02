ce6c78ec01f69427747018ef68c07b507dadb6bd
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-02 16:33:56 +0300

    creating user file

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name: {type: string, required: true},
+    email: {type: string, required: true, unique: true},
+    password: {type: string, required: true},
+    role: {type: string, enum:['owner', 'user'], default:'user'},
+    image: {type: string, default: ''},
+},{timestamps: true})
+
+const User = mongoose.model('user', userSchema)
+
+export default User
