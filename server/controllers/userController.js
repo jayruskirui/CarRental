b12796ce4d70a7fd7270fb3b31093e634b1acbe5
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-02 22:49:05 +0300

    user data api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -60,3 +60,15 @@ export const loginUser = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+
+//Get user data using Token (JWT)
+export const getUserData = async (req, res) => {
+    try {
+        const {user} = req;
+        res.json({success: true, user})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
