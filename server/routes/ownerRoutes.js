52c3216d04a704d51629a40cef3260e640afc349
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-04 17:13:20 +0300

    creating ownerRoutes for : API to List Owner Cars, API to Toggle car Availability, API to Delete car, API to get dashboard data & booking models

--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
 import {protect} from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
-import { addCar } from "../controllers/ownerController.js";
+import { addCar, getOwnerCars, toggleCarAvailability, deleteCar } from "../controllers/ownerController.js";
 import { changeRoleToOwner } from "../controllers/ownerController.js";
 
 const ownerRouter = express.Router();
 
 ownerRouter.post('/change-role', protect, changeRoleToOwner)
 ownerRouter.post('/add-car', upload.single('image'), protect, addCar)
+ownerRouter.get('/cars', protect, getOwnerCars)
+ownerRouter.post('/toggle-car', protect, toggleCarAvailability)
+ownerRouter.post('/delete-car', protect, deleteCar)
+
+
 
 export default ownerRouter;
