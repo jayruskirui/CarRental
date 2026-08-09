67aa4644fe1fa979d1286c0adb883d20fbb583c0
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-09 16:13:21 +0300

    creating an api for the dashboard

--- client/src/pages/owners/Layout.jsx
+++ client/src/pages/owners/Layout.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../../components/owner/Sidebar'
 import NavbarOwner from '../../components/owner/NavbarOwner'
+import { useAppContext } from '../../context/AppContext'
 
 const Layout = () => {
+
+  const {isOwner,navigate} = useAppContext()
+
+  useEffect(()=>{
+    if(!isOwner) {
+      navigate('/')
+    }
+  },[isOwner])
+
   return (
     <div className='min-h-screen bg-gray-50'>
       <NavbarOwner />
