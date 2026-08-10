f8ad3b51681d73736077cc35a1b27073d39f8ea5
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-11 00:17:57 +0300

    adding animations

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {assets, cityList} from '../assets/assets'
 import { useAppContext } from '../context/AppContext';
+import {delay, motion} from 'motion/react'
 
 const Hero = () => {
 
@@ -14,11 +15,20 @@ const handleSearch = (e) => {
 }
 
   return (
-    <div className = 'pt-10 flex flex-col items-center justify-center gap-14 bg-light text-center'>
+    <motion.div initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                transition={{duration: 0.8}}
+    className = 'pt-10 flex flex-col items-center justify-center gap-14 bg-light text-center'>
 
-        <h1 className = 'text-4xl md:text-5xl font-semibold'>Luxury Cars on Rent</h1>
+        <motion.h1  initial={{y: 50, opacity: 0}}
+                    animate={{y:0, opacity: 1}}
+                    transition={{duration: 0.8, delay: 0.2}}
+        className = 'text-4xl md:text-5xl font-semibold'>Luxury Cars on Rent</motion.h1>
 
-        <form onSubmit={handleSearch} 
+        <motion.form initial={{scale: 0.95, y: 50, opacity: 0}}
+                     animate={{scale: 1, y:0, opacity: 1}}
+                     transition={{duration: 0.6, delay: 0.4}}
+        onSubmit={handleSearch} 
         className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
             <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
                 
@@ -44,16 +54,21 @@ const handleSearch = (e) => {
                     className='text-sm text-gray-500' required/>
                 </div>
             </div>
-            <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary
+            <motion.button  whileHover={{scale: 1.05}}
+                            whileTap={{scale: 0.95}}
+            className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary
                 hover:bg-primary-dull text-white rounded-full cursor-pointer'>
                     <img src = {assets.search_icon} alt = "search"
                     className='brightness-300' />
                     Search
-            </button>
-        </form>
+            </motion.button>
+        </motion.form>
 
-        <img src = {assets.main_car} alt = 'car' className = 'max-h-74' />
-    </div>
+        <motion.img  initial={{y: 100, opacity: 0}}
+                     animate={{y:0, opacity: 1}}
+                     transition={{duration: 0.8, delay: 0.6}}
+        src = {assets.main_car} alt = 'car' className = 'max-h-74' />
+    </motion.div>
   )
 }
 
--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from './Title'
 import { assets } from '../assets/assets';
+import {motion} from 'motion/react'
 
 const Testimonials = () => {
 
@@ -34,7 +35,12 @@ const testimonials = [
 
             <div className="grid girid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
                 {testimonials.map((testimonial, index) => (
-                    <div key={index} className="bg-white p-6 rounded-xl shadow-lg hover:-translate-y-1 transition-all
+                    <motion.div 
+                    initial={{opacity: 0, y: 40}}
+                    whileInView={{opacity: 1, y: 0}}
+                    transition={{duration: 0.6, delay: index * 0.2, ease: "easeOut"}}
+                    viewport={{once: true, amount: 0.3}}
+                    key={index} className="bg-white p-6 rounded-xl shadow-lg hover:-translate-y-1 transition-all
                     duration-500">
 
                         <div className="flex items-center gap-3">
@@ -50,7 +56,7 @@ const testimonials = [
                             ))}
                         </div>
                         <p className="text-gray-500 max-w-90 mt-4 font-light">"{testimonial.testimonial}"</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
