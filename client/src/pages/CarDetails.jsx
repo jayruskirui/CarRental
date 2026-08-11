802447382eae20b3ef681f57613e3b6dd86db0bc
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-08-11 23:02:57 +0300

    amounting animation on cardetails, cars & mybookings pages

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -4,6 +4,7 @@ import { assets } from '../assets/assets'
 import Loader from '../components/Loader'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react'
 
 const CarDetails = () => {
 
@@ -49,9 +50,24 @@ useEffect (()=>{
 
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
           {/* Left: Car image and Details */}
-          <div className='lg:col-span-2'>
-             <img src = {car.image} alt = "" className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md' /> 
-             <div className='space-y-6'>
+          <motion.div 
+          initial={{opacity: 0, y: 30}}
+          animate={{opacity:1, y:0}}
+          transition={{duration: 0.6}}
+          
+          className='lg:col-span-2'>
+             <motion.img 
+              initial={{opacity: 0, scale: 0.98}}
+              animate={{opacity:1, scale:1}}
+              transition={{duration: 0.5}}
+             
+             src = {car.image} alt = "" className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md' /> 
+             <motion.div className='space-y-6'
+              initial={{opacity: 0}}
+              animate={{opacity:1}}
+              transition={{duration: 0.5, delay: 0.2}}
+             
+             >
               <div>
                 <h1 className='text-3xl font-bold'>{car.brand} {car.model}</h1>
                 <p className='text-gray-500 text-lg'>{car.category} · {car.year}</p>
@@ -65,10 +81,15 @@ useEffect (()=>{
                   {icon: assets.car_icon, text: car.transmission},
                   {icon: assets.location_icon, text: car.location},
                 ].map(({icon, text})=>(
-                  <div key={text} className='flex flex-col items-center bg-light p-4 rounded-lg'>
+                  <motion.div 
+                  initial={{opacity: 0, y: 10}}
+                  animate={{opacity:1, y:0}}
+                  transition={{duration: 0.4}}
+                  
+                   key={text} className='flex flex-col items-center bg-light p-4 rounded-lg'>
                     <img src={icon} alt="" className='h-5 mb-2'/>
                     {text}
-                  </div>
+                  </motion.div>
                 ))}
               </div> 
 
@@ -93,11 +114,16 @@ useEffect (()=>{
                   }
                 </ul>
               </div>
-             </div>
-          </div>
+             </motion.div>
+          </motion.div>
 
           {/* Right: Booking form */}
-          <form onSubmit={handleSubmit}
+          <motion.form 
+          initial={{opacity: 0, y: 30}}
+          animate={{opacity:1, y:0}}
+          transition={{duration: 0.6, delay: 0.3}}
+
+          onSubmit={handleSubmit}
           className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
             <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>
               {currency}{car.pricePerDay}
@@ -123,7 +149,7 @@ useEffect (()=>{
               font-medium text-white rounded-xl cursor-pointer'>Book Now</button>
 
               <p className='text-center text-sm'>No credit card required to reserve</p>
-          </form>
+          </motion.form>
         </div>
 
     </div>
--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 import Title from '../components/Title'
 import { useAppContext } from '../context/AppContext'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react'
 
 const MyBookings = () => {
 
@@ -29,7 +30,12 @@ useEffect(()=>{
 },[user])
 
   return (
-    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl'>
+    <motion.div 
+          initial={{opacity: 0, y: 30}}
+          animate={{opacity:1, y:0}}
+          transition={{duration: 0.6}}
+    
+    className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl'>
         
         <Title title='My Bookings'
         subTitle='View and manage your all car bookings'
@@ -37,7 +43,12 @@ useEffect(()=>{
 
         <div>
           {bookings.map((booking, index) => (
-            <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor
+            <motion.div 
+            initial={{opacity: 0, y: 20}}
+            animate={{opacity:1, y:0}}
+            transition={{duration: 0.4, delay:index * 0.1}}
+
+            key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor
              rounded-lg mt-5 first:mt-12'>
 
               {/* Car image + info */}
@@ -85,11 +96,11 @@ useEffect(()=>{
                 </div>
               </div>
 
-            </div>
+            </motion.div>
           ))}
         </div>
 
-    </div>
+    </motion.div>
   )
 }
 
